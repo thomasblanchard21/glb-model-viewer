f833482e131664f1c28cce22c3ab6bc43a0dcf3a
Thomas Blanchard <thomasblanchard@kinetix.tech>
2025-03-11 11:13:57 +0100

    Use url parameters instead of POST messages

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 import { initScene, animate } from "./sceneSetup.js";
 import { setupEventHandlers } from './eventHandler.js'
+import { loadModel } from "./modelLoader.js";
 
 initScene();
 setupEventHandlers();
+
+const url = new URL(window.location.href);
+const model = url.searchParams.get("model");
+loadModel(`https://gradio-model-viewer.s3.eu-west-1.amazonaws.com/models/${model}.glb`);
 
 animate();
--- modelLoader.js
+++ modelLoader.js
@@ -1,74 +1,66 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-import { scene, camera, renderer } from "./sceneSetup.js";
+import { scene } from "./sceneSetup.js";
 
 export let mixer = null;
 export let currentGltf = null;
 export let currentModel = null;
 
 export function loadModel(modelUrl) {
-    return new Promise((resolve, reject) => {
-        if (!modelUrl) {
-            reject("No model URL provided");
-            return;
-        }
+    if (!modelUrl) {
+        console.error("No model URL provided");
+        return;
+    }
 
-        removeModel();
+    removeModel();
 
-        const loadingManager = new THREE.LoadingManager();
+    const loadingManager = new THREE.LoadingManager();
 
-        loadingManager.onStart = function ( url, itemsLoaded, itemsTotal ) {
-            console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-        }
+    loadingManager.onStart = function (url, itemsLoaded, itemsTotal) {
+        console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+    }
 
-        loadingManager.onLoad = function ( ) {
-            console.log( 'Loading complete!');
-        }
+    loadingManager.onLoad = function () {
+        console.log('Loading complete!');
+    }
 
-        loadingManager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
-            console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-        }
+    loadingManager.onProgress = function (url, itemsLoaded, itemsTotal) {
+        console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+    }
 
-        loadingManager.onError = function ( url ) {
-            console.log( 'There was an error loading ' + url );
-        }
+    loadingManager.onError = function (url) {
+        console.log('There was an error loading ' + url);
+    }
 
-        console.log("Before:", scene.children);
+    console.log("Before:", scene.children);
 
-        const loader = new GLTFLoader(loadingManager);
-        loader.load(modelUrl, async function (gltf) {
-            currentGltf = gltf;
-            const model = gltf.scene;
-            model.traverse((node) => {
-                if (node.isMesh) {
-                    node.castShadow = true;
-                    node.receiveShadow = true;
-                }
-            });
-
-            currentModel = model;
-            await renderer.compile( model, camera, scene );
-            scene.add(model);
-
-            if (gltf.animations.length > 0) {
-                mixer = new THREE.AnimationMixer(currentModel);
-                const action = mixer.clipAction(gltf.animations[0]);
-                action.play();
+    const loader = new GLTFLoader(loadingManager);
+    loader.load(modelUrl, (gltf) => {
+        currentGltf = gltf;
+        const model = gltf.scene;
+        model.traverse((node) => {
+            if (node.isMesh) {
+                node.castShadow = true;
+                node.receiveShadow = true;
             }
-
-            console.log("After:", scene.children);
-
-            resolve();
-        },
-        function ( xhr ) {
-
-            console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-        },
-        undefined, (error) => {
-            console.error("Error loading model:", error);
-            reject(error);
         });
+
+        currentModel = model;
+        scene.add(model);
+
+        if (gltf.animations.length > 0) {
+            mixer = new THREE.AnimationMixer(currentModel);
+            const action = mixer.clipAction(gltf.animations[0]);
+            action.play();
+        }
+
+        console.log("After:", scene.children);
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    undefined, (error) => {
+        console.error("Error loading model:", error);
     });
 }
 
